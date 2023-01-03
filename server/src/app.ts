443467db3778dee5894e08e63441018b4c66a303
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-03 14:44:05 -0300

    feat: ativa o cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 
 import RouterOrders from './routes/orderRoutes';
@@ -5,6 +6,7 @@ import RouterOrders from './routes/orderRoutes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/orders', RouterOrders);
 
