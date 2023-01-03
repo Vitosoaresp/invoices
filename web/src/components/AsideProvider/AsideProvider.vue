4571ab3bafb3d18c0daacf221ac330f0984bb3c9
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-03 14:45:39 -0300

    refact: separa estilização do codigo

---
+++ web/src/components/AsideProvider/AsideProvider.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  import './styles.css';
+</script>
+
+<template>
+  <div class="container-aside">
+    <div class="cashforce-logo">
+      <img src="../../assets/cashforce.svg" alt="logo da cashforce">
+    </div>
+
+    <div class="menu">
+
+      <div class="menu-item">
+        <img src="../../assets/iconHand.svg" alt="icone de mão">
+        <p>Notas fiscais</p>
+      </div>
+
+    </div>
+  </div>
+</template>
