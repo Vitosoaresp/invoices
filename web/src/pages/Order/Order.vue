fd5cf94e038896e89b3220c78c288b4b6ff1c2fc
Vitor Soares <pereiravitor1218@gmail.com>
2023-01-03 15:14:54 -0300

    feat: page order

---
+++ web/src/pages/Order/Order.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import OrderCard from '../../components/OrderCard/OrderCard.vue';
+import { IOrder } from '../../interfaces/IOrder';
+
+import './styles.css';
+
+const data = ref(null as IOrder[] | null)
+const error = ref(null)
+
+fetch('http://localhost:3001/orders')
+  .then(response => response.json())
+  .then(json => (data.value = json))
+  .catch(err => (error.value = err))
+
+</script>
+
+<template>
+  <div class="container">
+    <header class="header"></header>
+
+    <main class="main">
+      <div class="title">
+        <img src="../../assets/iconHandDarkBlue.svg" alt="icone de uma mão">
+        <h2>Notas fiscais</h2>
+      </div>
+
+      <span>Visualize as notas fiscais que você tem.</span>
+
+      <div class="orders-container">
+        <div v-if="error" class="error">Não foi possivel buscar as notas!</div>
+        <table v-else-if="data" class="table">
+          <thead>
+            <tr class="thead-tr">
+              <th>NOTA FISCAL</th>
+              <th>SACADO</th>
+              <th>CEDENTE</th>
+              <th>EMISSÃO</th>
+              <th>VALOR</th>
+              <th>STATUS</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody class="tbody">
+            <OrderCard
+              v-for="order in data"
+              :key="order.orderNfId"
+              :orderNfId="order.orderNfId"
+              :createdAt="order.createdAt"
+              :buyer="order.buyer"
+              :provider="order.provider"
+              :value="order.value"
+              :orderStatusBuyer="order.orderStatusBuyer"
+            />
+          </tbody>
+        </table>
+        <div v-else class="loading">Carregando...</div>
+      </div>
+    </main>
+  </div>
+</template>
